885942d9b4c7a28a774971a236a0fa29258c98ab
Jonas <jromog@usal.es>
2025-10-23 14:52:03 +0200

    EntregaFinal

--- entrega5/src/App.java
+++ entrega5/src/App.java
@@ -3,6 +3,7 @@ import com.coti.tools.Esdia;
 import modelo.Director;
 import modelo.Pelicula;
 import modelo.Videoteca;
+ 
 
 public class App {
     public static void main(String[] args) throws Exception {
